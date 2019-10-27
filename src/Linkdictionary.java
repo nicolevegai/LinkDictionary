a6a4b0ad89588e9f3b9d47d76f1657f879dc544d
nicolevegai <mvega.ieu2017@student.ie.edu>
2019-10-27 13:14:33 +0100

    First Commit Final rep

---
+++ src/Linkdictionary.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Linkdictionary {
+
+    public static void main(String args[]) throws NumberFormatException {
+
+        BufferedReader reader;
+        LinkedList<String> dict = new LinkedList<String>();
+        try {
+            //
+            reader = new BufferedReader(new FileReader("/Users/nicolevega/LinkDictionary_Pair/unsorteddict.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                if (dict.size() == 0) {
+                    dict.add(line);
+                } else {
+                    if (!sortList(line, dict)) {
+                        dict.add(line);
+                    }
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+            BufferedWriter writer = new BufferedWriter(new FileWriter("prueba3.txt"));
+            for (int i = 0; i < dict.size(); i++) {
+                System.out.println(dict.get(i));
+                writer.write(dict.get(i).toString());
+                writer.newLine();
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
