ebbbf28626a15dda196acbfbe57546d6c489fdfd
nicolevegai <mvega.ieu2017@student.ie.edu>
2019-10-27 14:03:09 +0100

    unsorteddict

--- src/Linkdictionary.java
+++ src/Linkdictionary.java
@@ -8,12 +8,12 @@ public class Linkdictionary {
 
     public static void main(String args[]) throws NumberFormatException {
         long startTime = System.nanoTime();
-        
+
         BufferedReader reader;
         LinkedList<String> dict = new LinkedList<String>();
         try {
             //
-            reader = new BufferedReader(new FileReader("/Users/nicolevega/LinkDictionary_Pair/unsorteddict.txt"));
+            reader = new BufferedReader(new FileReader("unsorteddict.txt"));
             String line = reader.readLine();
             while (line != null) {
                 if (dict.size() == 0) {
@@ -26,7 +26,7 @@ public class Linkdictionary {
                 line = reader.readLine();
             }
             reader.close();
-            BufferedWriter writer = new BufferedWriter(new FileWriter("prueba3.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("sorteddict.txt"));
             for (int i = 0; i < dict.size(); i++) {
                 System.out.println(dict.get(i));
                 writer.write(dict.get(i).toString());
